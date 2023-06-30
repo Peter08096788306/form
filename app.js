612fe2dc0d69ba65885dcd82bfa6b2c6ad8d2f85
Oyinloye Peter <oyinloyepeter273@gmail.com>
2023-06-30 08:05:43 -0700

    Some text changed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const display = document.querySelector(".display");
 const gray = document.querySelector(".Gray--color");
 const form = document.querySelector(".form");
-const empty1 = document.querySelector(".empty");
+const empty = document.querySelector(".empty");
+const empty1 = document.querySelector(".empty1");
 const empty2 = document.querySelector(".empty2");
 const display2 = document.querySelector(".display2");
+const logln = document.querySelector(".Login--form");
 
 const form2 = document.querySelector(".form2");
 const text = document.querySelector(".text");
@@ -12,11 +14,14 @@ const para = document.querySelector(".para");
 
 display.addEventListener('click', () => {
     form2.style.display = "none"
-    empty2.style.display = "block"
+    empty.style.display = "block"
     empty1.style.display = "block"
+    empty2.style.display = "none"
     text.style.display = "none"
     icon.style.display = "none"
     form.classList.add("open")
+    logln.classList.add("gin")
+    // empty2.classList.add("none")
     display.classList.add("change")
     display2.classList.add("gray")
     form.classList.add("change")
@@ -32,11 +37,14 @@ display.addEventListener('click', () => {
 
 display2.addEventListener("click", () => {
     form2.classList.add("open")
+    empty1.style.display = "none"
     empty2.style.display = "block"
     form2.style.display = "block"
     icon.style.display = "block"
     text.style.display = "block"
+    // empty2.classList.add("none")
     if (form.classList.contains("open")) {
+        empty2.classList.remove("none")
         display2.classList.remove("gray")
         form.classList.remove("open")
         gray.classList.add("red")
